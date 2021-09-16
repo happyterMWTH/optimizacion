81b72002c6fce609d653e9527881f68318957539
happyterMWTH <62663250+happyterMWTH@users.noreply.github.com>
2021-09-16 12:00:09 -0500

    final
    :)

--- ajuste_cuadratico.m
+++ ajuste_cuadratico.m
@@ -13,66 +13,107 @@ Int = [x1 x2 x3]; % Intervalo inicial
 k = 0; % Contador de iteraciones
 syms x;
 func(x) = (x^2)/10 - 2*sin(x);% Función
+fplot(func,[-5 5]);
+title('Método de Ajuste Cuadrático')
+hold on;
 
 % Estimación de x_bar
 
-b_23= (x2)^2 - (x3)^2
-b_31= (x3)^2 - (x1)^2
-b_12= (x1)^2 - (x2)^2
-a_23= x2 - x3
-a_31= x3 - x1
-a_12= x1 - x2
-fx1 = double(func(x1))
-fx2 = double(func(x2))
-fx3 = double(func(x3))
+b_23= (x2)^2 - (x3)^2;
+b_31= (x3)^2 - (x1)^2;
+b_12= (x1)^2 - (x2)^2;
+a_23= x2 - x3;
+a_31= x3 - x1;
+a_12= x1 - x2;
+fx1 = double(func(x1));
+fx2 = double(func(x2));
+fx3 = double(func(x3));
 
 
-xbar_k = double((b_23*fx1+b_31*fx2+b_12*fx3)/(2*(a_23*fx1+a_31*fx2+a_12*fx3)))
-f_xbar_k = double(func(xbar_k))
+xbar_k = double((b_23*fx1+b_31*fx2+b_12*fx3)/(2*(a_23*fx1+a_31*fx2+a_12*fx3)));
+f_xbar_k = double(func(xbar_k));
+disp('RESULTADOS MÉTODO AJUSTE CUADRÁTICO');
+disp('It.    x_bar       x1          x3');
+mat = [];
 
 for a = 1:1:100
     % Paso 1
-    if abs(x3 - x1) < len_final
-        xbar_k = double(xbar_k)
-        x1 = double(x1)
-        x3 = double(x3)
-        k
+    if x3 - x1 < len_final
+        xbar_k = double(xbar_k);
+        x1 = double(x1);
+        x3 = double(x3);
+        k;
         break;
     else
         if xbar_k > x2
             %Paso 2
-            if f_xbar_k > double(func(x2))
-                Int = [x1 x2 xbar_k]
+            if f_xbar_k > fx2
+                Int = [x1 x2 xbar_k];
             else 
-                Int = [x2 xbar_k x3]
-            x1 = Int(1,1)
-            x2 = Int(1,2)
-            x3 = Int(1,3)
+                Int = [x2 xbar_k x3];
             end
+            x1 = Int(1,1);
+            x2 = Int(1,2);
+            x3 = Int(1,3);
+            
+            b_23= (x2)^2 - (x3)^2;
+            b_31= (x3)^2 - (x1)^2;
+            b_12= (x1)^2 - (x2)^2;
+            a_23= x2 - x3;
+            a_31= x3 - x1;
+            a_12= x1 - x2;
+            fx1 = double(func(x1));
+            fx2 = double(func(x2));
+            fx3 = double(func(x3));
+
+
+            xbar_k = double((b_23*fx1+b_31*fx2+b_12*fx3)/(2*(a_23*fx1+a_31*fx2+a_12*fx3)));
+        
 
         elseif xbar_k < x2
             %Paso 3
-            if f_xbar_k > double(func(x2))
-                Int = [xbar_k x2 x3]
+            if f_xbar_k > fx2
+                Int = [xbar_k x2 x3];
             else 
-                Int = [x1 xbar_k x2]
-            x1 = Int(1,1)
-            x2 = Int(1,2)
-            x3 = Int(1,3)
+                Int = [x1 xbar_k x2];
             end
+            x1 = Int(1,1);
+            x2 = Int(1,2);
+            x3 = Int(1,3);
+            
+            b_23= (x2)^2 - (x3)^2;
+            b_31= (x3)^2 - (x1)^2;
+            b_12= (x1)^2 - (x2)^2;
+            a_23= x2 - x3;
+            a_31= x3 - x1;
+            a_12= x1 - x2;
+            fx1 = double(func(x1));
+            fx2 = double(func(x2));
+            fx3 = double(func(x3));
+
+
+            xbar_k = double((b_23*fx1+b_31*fx2+b_12*fx3)/(2*(a_23*fx1+a_31*fx2+a_12*fx3)));
+        
             
         elseif xbar_k == x2
             %Paso 4
-            if x2 - x1 < x3 - x2 
-                xbar_k = x2 + len_final/2
-            elseif x2 - x1 >= x3 - x2
-                xbar_k = x2 - len_final/2
+            if abs(x2 - x1) < abs(x3 - x2)
+                xbar_k = x2 + len_final/2;
+            elseif abs(x2 - x1) > abs(x3 - x2)
+                xbar_k = x2 - len_final/2;
             end
         else
             disp('error');
             break
         end
-    
     end
     k = k + 1;
+    f_xbar_k = double(func(xbar_k));
+    fx2 = double(func(x2));
+    mat = [ mat; k double(xbar_k) double(x1) double(x3) ];
 end
+
+scatter(xbar_k,func(xbar_k),'ro');
+vec_final = [double(xbar_k) double(func(xbar_k))];
+legend('f(x)', num2str(vec_final));
+disp(num2str(mat));
--- metodo_newton.m
+++ metodo_newton.m
@@ -10,19 +10,34 @@ k = 0; % Contador de iteraciones
 err = 0.00001; %error
 syms x;
 func(x) = (x^2)/10 - 2*sin(x);% Función
+figure
+fplot(func,[-5 5]);
+title('Método de Newton')
+hold on;
+mat = [];
 
 while 1
-    dx_1(x) = diff(func,x) %Primera derivada
-    dx_2(x) = diff(dx_1,x) %Segunda derivada
-    dev1_evalxk = double(dx_1(xk)) %Primera derivada evaluada en xk
-    dev2_evalxk = double(dx_2(xk)) %Segunda derivada evaluada en xk
-    x_k1 = xk - dev1_evalxk/dev2_evalxk %x_(k + 1);
+    dx_1(x) = diff(func,x); %Primera derivada
+    dx_2(x) = diff(dx_1,x); %Segunda derivada
+    dev1_evalxk = double(dx_1(xk)); %Primera derivada evaluada en xk
+    dev2_evalxk = double(dx_2(xk)); %Segunda derivada evaluada en xk
+    x_k1 = xk - dev1_evalxk/dev2_evalxk; %x_(k + 1);
     if abs(x_k1 - xk) < err
-        k
-        xk = double(xk)
-        x_k1 = double(x_k1)
+        k;
+        xk = double(xk);
+        x_k1 = double(x_k1);
         break
     end
     xk = x_k1;
     k = k + 1;
+    mat = [mat; k xk x_k1 ];
 end
+
+scatter(xk,func(xk),'ro');
+scatter(x_k1, func(x_k1),'bo');
+vec_final1 = [double(xk) double(func(xk))];
+vec_final2 = [double(x_k1) double(func(x_k1))];
+legend('f(x)', num2str(vec_final1), num2str(vec_final2));
+disp('RESULTADOS MÉTODO DE NEWTON');
+disp('It.      xk        x_k+1');
+disp(num2str(mat));
